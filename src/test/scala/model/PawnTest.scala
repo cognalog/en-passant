e42132c9106e7f84f48a961701c068e3281caebf
Tyrone Hinderson <cognalog@users.noreply.github.com>
2020-12-24 00:29:10 +0100

    Add starting 2-square move for pawn
    Also, add "has moved" flag to Piece. This will be useful for Rook and King to determine castle eligibility.

--- src/test/scala/model/PawnTest.scala
+++ src/test/scala/model/PawnTest.scala
@@ -3,28 +3,37 @@ package model
 import org.scalatest.FunSuite
 
 class PawnTest extends FunSuite {
-  test("testGetLegalMoves_Upward") {
+
+  test("testGetLegalMoves_FirstMove") {
     val board = new Board(Map())
-    val pawn  = new Pawn(Color.White)
+    val pawn = new Pawn(Color.White, false)
+    assertResult(Set(Square(4, 3), Square(4, 4))) {
+      pawn.getLegalMoves(Square(4, 2), board)
+    }
+  }
+
+  test("testGetLegalMoves_OneForward") {
+    val board = new Board(Map())
+    val pawn = new Pawn(Color.White, true)
     assertResult(Set(Square(4, 3))) {
       pawn.getLegalMoves(Square(4, 2), board)
     }
   }
 
-  test("testGetLegalMoves_UpwardOutOfBounds") {
+  test("testGetLegalMoves_OneForwardOutOfBounds") {
     val board = new Board(Map())
-    val pawn  = new Pawn(Color.White)
+    val pawn = new Pawn(Color.White, true)
     assertResult(Set()) {
       pawn.getLegalMoves(Square(4, 8), board)
     }
   }
 
-  test("testGetLegalMoves_UpwardBlockedByPiece") {
+  test("testGetLegalMoves_OneForwardBlockedByPiece") {
     val board =
       new Board(
         Map(Square(4, 3) -> new Pawn(Color.Black))
       )
-    val pawn = new Pawn(Color.White)
+    val pawn = new Pawn(Color.White, true)
     assertResult(Set()) {
       pawn.getLegalMoves(Square(4, 2), board)
     }
@@ -37,7 +46,7 @@ class PawnTest extends FunSuite {
         Square(5, 3) -> new Pawn(Color.White)
       )
     )
-    val pawn = new Pawn(Color.White)
+    val pawn = new Pawn(Color.White, true)
     assertResult(Set(Square(4, 3))) {
       pawn.getLegalMoves(Square(4, 2), board)
     }
@@ -48,7 +57,7 @@ class PawnTest extends FunSuite {
       new Board(
         Map(Square(3, 3) -> new Pawn(Color.Black))
       )
-    val pawn = new Pawn(Color.White)
+    val pawn = new Pawn(Color.White, true)
     assertResult(Set(Square(4, 3), Square(3, 3))) {
       pawn.getLegalMoves(Square(4, 2), board)
     }
@@ -59,7 +68,7 @@ class PawnTest extends FunSuite {
       new Board(
         Map(Square(5, 3) -> new Pawn(Color.Black))
       )
-    val pawn = new Pawn(Color.White)
+    val pawn = new Pawn(Color.White, true)
     assertResult(Set(Square(4, 3), Square(5, 3))) {
       pawn.getLegalMoves(Square(4, 2), board)
     }
@@ -67,7 +76,7 @@ class PawnTest extends FunSuite {
 
   test("testGetLegalMoves_EnPassant") {
     val board = new Board(Map(), enPassant = Some(Square(5, 3)))
-    val pawn = new Pawn(Color.White)
+    val pawn = new Pawn(Color.White, true)
     assertResult(Set(Square(4, 3), Square(5, 3))) {
       pawn.getLegalMoves(Square(4, 2), board)
     }

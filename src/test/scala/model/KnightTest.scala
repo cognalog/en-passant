2c98f1bf473295b4beea05655d8694c24b2141a7
Tyrone Hinderson <tymail707@gmail.com>
2021-01-01 10:45:54 -0500

    Implement Knight

---
+++ src/test/scala/model/KnightTest.scala
@@ -0,0 +1,23 @@
+package model
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class KnightTest extends AnyFunSuite {
+
+  test("testGetLegalMoves") {
+    val board = new Board(Map(), Color.White)
+    val knight = Knight(Color.White)
+    assertResult(Set(Square(5, 6), Square(6, 5), Square(2, 5), Square(5, 2), Square(3, 6), Square(6, 3), Square(2, 3), Square(3, 2))) {
+      knight.getLegalMoves(Square(4, 4), board)
+    }
+  }
+
+  test("testGetLegalMoves_SomeOOBFromRim") {
+    val board = new Board(Map(), Color.White)
+    val knight = Knight(Color.White)
+    assertResult(Set(Square(2, 6), Square(2,2), Square(3, 5), Square(3, 3))) {
+      knight.getLegalMoves(Square(1, 4), board)
+    }
+  }
+
+}

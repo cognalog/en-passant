de6bb284800731734c3d634d4407a104891808d2
cognalog <tymail707@gmail.com>
2021-07-17 11:55:09 +0200

    Create Game companion object for quickly initializing games
    We can add a function that asks for input when we have some more
    options for Evaluator and MoveSearch impls.

--- src/main/scala/actor/Game.scala
+++ src/main/scala/actor/Game.scala
@@ -1,10 +1,27 @@
 package actor
 
+import ai.evaluator.RandomEvaluator
+import ai.search.Minimax
 import model.Color.Color
-import model.{Board, StandardBoard}
+import model.{Board, Color, StandardBoard}
 
 import scala.collection.mutable.ListBuffer
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Random, Success, Try}
+
+object Game {
+  private val minimax_depth = 3
+  private val botGameMaxTurns = 25
+
+  def withRandomColors(player1: Player, player2: Player, maxTurns: Int = Int.MaxValue): Game = {
+    val color1 = if (Random.nextBoolean()) Color.White else Color.Black
+    Game(Map(color1 -> player1, Color.opposite(color1) -> player2), maxTurns)
+  }
+
+  def humanVsBot: Game = withRandomColors(HumanPlayer(), BotPlayer(Minimax(minimax_depth, RandomEvaluator)))
+
+  def botVsBot: Game = withRandomColors(BotPlayer(Minimax(minimax_depth, RandomEvaluator)),
+    BotPlayer(Minimax(minimax_depth, RandomEvaluator)), botGameMaxTurns)
+}
 
 case class Game(players: Map[Color, Player], maxTurns: Int = Int.MaxValue) {
 
@@ -12,7 +29,7 @@ case class Game(players: Map[Color, Player], maxTurns: Int = Int.MaxValue) {
     var turns = 0
     val boardLog: ListBuffer[Board] = ListBuffer()
     var currentBoard: Board = StandardBoard.StartingPosition
-    while (currentBoard.getNextMoves.nonEmpty && turns < maxTurns * 2) {
+    while (currentBoard.getNextMoves.nonEmpty && turns / 2 < maxTurns) {
       turns += 1
       boardLog.append(currentBoard)
       val turnColor = currentBoard.turnColor

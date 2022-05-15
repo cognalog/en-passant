ee17d9245598c9ba7d9cb4026773c493194e5c83
Tyrone Hinderson <tymail707@gmail.com>
2022-05-15 15:19:09 -0400

    Implement ability to generate standard notation for a Move
    This will be necessary for the service (as currently designed).

--- src/main/scala/model/Move.scala
+++ src/main/scala/model/Move.scala
@@ -5,6 +5,9 @@ import scala.util.{Failure, Success, Try}
 object Move {
   private val normalMovePattern = raw"([BKNQR])?([a-h])?x?([a-h][1-8])(=[BNQR])?\+?".r
 
+  private val kingsideCastleNotation = "O-O"
+  private val queensideCastleNotation = "O-O-O"
+
   /**
    * Parse a move string written in Algebraic Notation. Limited legality
    * checking is performed to resolve ambiguity.
@@ -25,7 +28,7 @@ object Move {
           case King.standardNotation   => King(board.turnColor)
           case Knight.standardNotation => Knight(board.turnColor)
           case Queen.standardNotation  => Queen(board.turnColor)
-          case Rook.standardNotation  => Rook(board.turnColor)
+          case Rook.standardNotation   => Rook(board.turnColor)
           case null                    => Pawn(board.turnColor)
         }
         val startSquares = board.locatePiece(pieceToFind).filter(
@@ -40,10 +43,62 @@ object Move {
         if (startSquares.size > 1) return Failure(new IllegalArgumentException(s"Ambiguous move: $move"))
         startSquares.headOption match {
           case Some(start) => Success(NormalMove(start, parsedDest, promotionPiece))
-          case None => Failure(new IllegalArgumentException(s"Impossible move: $move"))
+          case None        => Failure(new IllegalArgumentException(s"Impossible move: $move"))
         }
       })
     case _ => Failure(new IllegalArgumentException(s"Malformed move: $move"))
+  }
+
+  /**
+   * Produce the standard notation for a given move on a given board.
+   *
+   * The result will be unambiguous. Some, but not all impossible moves will fail; ergo, do not rely upon this method
+   * for move validation.
+   *
+   * @see https://en.wikipedia.org/wiki/Algebraic_notation_(chess)
+   * @param move  the move to translate.
+   * @param board the board in whose context to describe the move.
+   * @return the move string in standard notation. */
+  def toStandardNotation(move: Move, board: Board): Try[String] = {
+    val checkIndicator = board.move(move) match {
+      case Success(board) if board.kingInCheck(board.turnColor) => "+"
+      case Success(_)                                           => ""
+      case Failure(ex)                                          => return Failure(ex)
+    }
+    move match {
+      case NormalMove(start, dest, maybePromo) => val capturePart =
+        board.pieceAt(dest) match {
+          case Some(piece: Piece) if piece.isColor(Color.opposite(board.turnColor)) => "x"
+          case _                                                                    => ""
+        }
+        board.pieceAt(start) match {
+          case Some(Pawn(_, _))   => val fileString = if (capturePart.nonEmpty) start.standardFileName else ""
+            val promoPart = maybePromo match {
+              case Some(piece: Piece) => s"=${piece.shortName}"
+              case _                  => ""
+            }
+            Success(s"$fileString$capturePart${move.destination.standardName}$promoPart$checkIndicator")
+          case Some(piece: Piece) => val disambiguation = disambiguatingString(move.asInstanceOf[NormalMove], board)
+            Success(s"${piece.shortName}$disambiguation$capturePart${move.destination.standardName}$checkIndicator")
+          case None               => Failure(new IllegalArgumentException(s"No piece at $start"))
+        }
+      case CastleMove(Square(file, _))         => Success(
+        if (file == 7) kingsideCastleNotation else queensideCastleNotation)
+      case _                                   => Failure(new IllegalArgumentException(s"Illegal move $move"))
+    }
+  }
+
+  private def disambiguatingString(move: NormalMove, board: Board): String = board.pieceAt(move.start) match {
+    case None               => ""
+    case Some(piece: Piece) => val otherPossibleStarts = board.locatePiece(piece).filter(
+      square => square != move.start &&
+                piece.getLegalMoves(square, board).contains(NormalMove(square, move.destination)))
+      if (otherPossibleStarts.isEmpty) return ""
+      val fileAmbiguous = otherPossibleStarts.exists(square => square.file == move.start.file)
+      val rankAmbiguous = otherPossibleStarts.exists(square => square.rank == move.start.rank)
+      val fileStr = if (!fileAmbiguous || rankAmbiguous) move.start.standardFileName else ""
+      val rankStr = if (fileAmbiguous) move.start.rank else ""
+      s"$fileStr$rankStr"
   }
 }
 
--- src/test/scala/model/MoveTest.scala
+++ src/test/scala/model/MoveTest.scala
@@ -14,7 +14,15 @@ class MoveTest extends AnyFunSuite {
   test("testFromStandardNotation_impossible") {
     assertResult("Impossible move: h8") {
       Move.fromStandardNotation("h8", StandardBoard(Map(Square(1, 1) -> Pawn(Color.White))))
-        .fold(ex => ex.getMessage, _ => fail())
+          .fold(ex => ex.getMessage, _ => fail())
+    }
+  }
+
+  test("testToStandardNotation_impossible") {
+    assertResult("There's no piece at Square(8,7)") {
+      Move.toStandardNotation(
+        NormalMove(Square(8, 7), Square(8, 8)), StandardBoard(Map(Square(1, 1) -> Pawn(Color.White))))
+          .fold(ex => ex.getMessage, _ => fail())
     }
   }
 
@@ -24,9 +32,25 @@ class MoveTest extends AnyFunSuite {
     }
   }
 
+  test("testToStandardNotation_whiteKingsideCastle") {
+    assertResult(Success("O-O")) {
+      Move.toStandardNotation(
+        CastleMove(Square(7, 1)), StandardBoard(
+          Map(Square(5, 1) -> King(Color.White), Square(8, 1) -> Rook(Color.White)), turnColor = Color.White))
+    }
+  }
+
   test("testFromStandardNotation_whiteQueensideCastle") {
     assertResult(Success(CastleMove(Square(3, 1)))) {
       Move.fromStandardNotation("O-O-O", StandardBoard(Map(), turnColor = Color.White))
+    }
+  }
+
+  test("testToStandardNotation_whiteQueensideCastle") {
+    assertResult(Success("O-O-O")) {
+      Move.toStandardNotation(
+        CastleMove(Square(3, 1)), StandardBoard(
+          Map(Square(5, 1) -> King(Color.White), Square(1, 1) -> Rook(Color.White)), turnColor = Color.White))
     }
   }
 
@@ -42,38 +66,94 @@ class MoveTest extends AnyFunSuite {
     }
   }
 
+  test("testToStandardNotation_blackQueensideCastle") {
+    assertResult(Success("O-O-O")) {
+      Move.toStandardNotation(
+        CastleMove(Square(3, 8)), StandardBoard(
+          Map(Square(5, 8) -> King(Color.Black), Square(1, 8) -> Rook(Color.Black)), turnColor = Color.Black))
+    }
+  }
+
+
   test("testFromStandardNotation_standardOpeningPawnMove") {
     assertResult(Success(NormalMove(Square(4, 2), Square(4, 4)))) {
       Move.fromStandardNotation("d4", StandardBoard.StartingPosition)
     }
   }
 
+  test("testToStandardNotation_standardOpeningPawnMove") {
+    assertResult(Success("d4")) {
+      Move.toStandardNotation(NormalMove(Square(4, 2), Square(4, 4)), StandardBoard.StartingPosition)
+    }
+  }
+
   test("testFromStandardNotation_pawnCaptureVsMove") {
     assertResult(Success(NormalMove(Square(4, 4), Square(5, 5)))) {
-      Move.fromStandardNotation("dxe5", StandardBoard(
-        Map(Square(5, 4) -> Pawn(Color.White), Square(4, 4) -> Pawn(Color.White), Square(5, 5) -> Pawn(Color.Black))))
+      Move.fromStandardNotation(
+        "dxe5", StandardBoard(
+          Map(Square(5, 4) -> Pawn(Color.White), Square(4, 4) -> Pawn(Color.White), Square(5, 5) -> Pawn(Color.Black))))
+    }
+  }
+
+  test("testToStandardNotation_pawnCaptureVsMove") {
+    assertResult(Success("dxe5")) {
+      Move.toStandardNotation(
+        NormalMove(Square(4, 4), Square(5, 5)), StandardBoard(
+          Map(Square(5, 4) -> Pawn(Color.White), Square(4, 4) -> Pawn(Color.White), Square(5, 5) -> Pawn(Color.Black))))
     }
   }
 
   test("testFromStandardNotation_pieceObstructingItsTwin") {
     assertResult(Success(NormalMove(Square(3, 3), Square(3, 8)))) {
-      Move.fromStandardNotation("Rc8", StandardBoard(
-        Map(Square(3, 2) -> Rook(Color.White), Square(3, 3) -> Rook(Color.White))))
+      Move.fromStandardNotation(
+        "Rc8", StandardBoard(Map(Square(3, 2) -> Rook(Color.White), Square(3, 3) -> Rook(Color.White))))
     }
   }
 
-  test("testFromStandardNotation_startColSpecified") {
+  test("testToStandardNotation_pieceObstructingItsTwin") {
+    assertResult(Success("Rc8")) {
+      Move.toStandardNotation(
+        NormalMove(Square(3, 3), Square(3, 8)),
+        StandardBoard(Map(Square(3, 2) -> Rook(Color.White), Square(3, 3) -> Rook(Color.White))))
+    }
+  }
+
+  test("testFromStandardNotation_startFileSpecified") {
     assertResult(Success(NormalMove(Square(4, 5), Square(2, 6)))) {
-      Move.fromStandardNotation("Ndb6", StandardBoard(
-        Map(Square(1, 4) -> Knight(Color.White), Square(4, 5) -> Knight(Color.White))))
+      Move.fromStandardNotation(
+        "Ndb6", StandardBoard(Map(Square(1, 4) -> Knight(Color.White), Square(4, 5) -> Knight(Color.White))))
+    }
+  }
+
+  test("testToStandardNotation_startFileSpecified") {
+    assertResult(Success("Ndb6")) {
+      Move.toStandardNotation(
+        NormalMove(Square(4, 5), Square(2, 6)),
+        StandardBoard(Map(Square(1, 4) -> Knight(Color.White), Square(4, 5) -> Knight(Color.White))))
+    }
+  }
+
+  test("testToStandardNotation_rankSpecified") {
+    assertResult(Success("Q4e1")) {
+      Move.toStandardNotation(
+        NormalMove(Square(8, 4), Square(5, 1)),
+        StandardBoard(Map(Square(8, 4) -> Queen(Color.White), Square(8, 1) -> Queen(Color.White))))
+    }
+  }
+
+  test("testToStandardNotation_fileAndRankSpecified") {
+    assertResult(Success("Qh4e1")) {
+      Move.toStandardNotation(
+        NormalMove(Square(8, 4), Square(5, 1)),
+        StandardBoard(Map(
+          Square(8, 4) -> Queen(Color.White), Square(5, 4) -> Queen(Color.White), Square(8, 1) -> Queen(Color.White))))
     }
   }
 
   test("testFromStandardNotation_ambiguous") {
     assertResult("Ambiguous move: Nb6") {
       Move.fromStandardNotation(
-        "Nb6", StandardBoard(
-          Map(Square(1, 4) -> Knight(Color.White), Square(4, 5) -> Knight(Color.White))))
+        "Nb6", StandardBoard(Map(Square(1, 4) -> Knight(Color.White), Square(4, 5) -> Knight(Color.White))))
           .fold(t => t.getMessage, _ => fail())
     }
   }
@@ -81,8 +161,15 @@ class MoveTest extends AnyFunSuite {
   test("testFromStandardNotation_promotion") {
     assertResult(Success(NormalMove(Square(5, 7), Square(5, 8), Some(Queen(Color.White))))) {
       Move.fromStandardNotation(
-        "e8=Q+", StandardBoard(
-          Map(Square(5, 7) -> Pawn(Color.White), Square(5, 2) -> King(Color.Black))))
+        "e8=Q+", StandardBoard(Map(Square(5, 7) -> Pawn(Color.White), Square(5, 2) -> King(Color.Black))))
+    }
+  }
+
+  test("testToStandardNotation_promotionWithCheck") {
+    assertResult(Success("e8=Q+")) {
+      Move.toStandardNotation(
+        NormalMove(Square(5, 7), Square(5, 8), Some(Queen(Color.White))),
+        StandardBoard(Map(Square(5, 7) -> Pawn(Color.White), Square(5, 2) -> King(Color.Black))))
     }
   }
 }
